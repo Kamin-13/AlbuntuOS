6257b80c1131a40fa0ca5e7477dc0974911a5236
Kamin <96984133+Kamin-13@users.noreply.github.com>
2024-03-09 17:27:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-#Later
+mkdir UwUassets
+cd UwUassets
+
+wget https://raw.githubusercontent.com/Kamin-13/AlbuntuOS/main/assets/test.png
+#Get Background image
+
+kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "UwUassets/test.png"
