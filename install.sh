1e1aca9818cb447f6b4958dbf79ae92c8933ac93
Kamin <96984133+Kamin-13@users.noreply.github.com>
2024-03-13 19:41:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 mkdir UwUassets
 cd UwUassets
 
-wget https://raw.githubusercontent.com/Kamin-13/AlbuntuOS/main/assets/test.png
+wget https://raw.githubusercontent.com/Kamin-13/AlbuntuOS/main/assets/bg.png
 #Get Background image
 
-kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "test.png"
+kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "bg.png"
 #I dont think this line works
 
 sudo curl https://raw.githubusercontent.com/Kamin-13/AlbuntuOS/main/assets/plasma.svgz -o /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/splash/images/plasma.svgz
